e62feda2f84e605856a1f1a7da7c5e08d5669dfe
Adam <adammarcaida06261996@gmail.com>
2023-03-02 23:58:14 +0800

    removed console.log

--- config/client.config.js
+++ config/client.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   port: 3000,
   baseUrl: "https://neptune-wrms-admin.onrender.com",
-  // corsConfig: {
-  //   origin: "https://neptune-wrms-admin.onrender.com",
-  //   credentials: true,
-  // },
   corsConfig: {
-    origin: "http://localhost:3000",
+    origin: "https://neptune-wrms-admin.onrender.com",
     credentials: true,
   },
+  // corsConfig: {
+  //   origin: "http://localhost:3000",
+  //   credentials: true,
+  // },
   // ngrokConfig: {
   //   origin: "https://9e3c-49-151-103-131.ap.ngrok.io",
   //   credentials: true,
